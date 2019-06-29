bbf784900d39984cf8bc6b55090638bf25edb966
Scott Silverman <scottms724@gmail.com>
2019-06-29 16:58:34 +0000

    Automatically backed up by Learn

--- db/01_create_users_table.rb
+++ db/01_create_users_table.rb
@@ -1,7 +1,10 @@
 class CreateUsersTable < ActiveRecord::Migration 
   
   def change
-    
+    create_table :users do |t|
+      t.string :username 
+      t.string :password
+      t.float :balance 
   end 
   
   
